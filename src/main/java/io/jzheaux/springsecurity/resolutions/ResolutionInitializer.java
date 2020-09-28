e9c4046fc7b4c878a0d156d5af28b44a2ec4a93f
frdupe <francis.dupe@capgemini.com>
2020-09-28 16:19:26 +0200

    module 1

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -1,22 +1,39 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
-	private final ResolutionRepository resolutions;
-
-	public ResolutionInitializer(ResolutionRepository resolutions) {
-		this.resolutions = resolutions;
-	}
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ResolutionRepository resolutionRepository;
 
 	@Override
 	public void afterSingletonsInstantiated() {
-		this.resolutions.save(new Resolution("Read War and Peace", "user"));
-		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
-		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+		this.resolutionRepository.save(new Resolution("Read War and Peace", "user"));
+		this.resolutionRepository.save(new Resolution("Free Solo the Eiffel Tower", "user"));
+		this.resolutionRepository.save(new Resolution("Hang Christmas Lights", "user"));
+		
+		User user = new User("user", "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		user.grantAuthority("resolution:read");
+		user.grantAuthority("resolution:write");
+		this.userRepository.save(user);
+		
+		User hasread = new User();
+		hasread.setUsername("hasread");
+		hasread.setPassword("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		hasread.grantAuthority("resolution:read");
+		this.userRepository.save(hasread);
+		
+		User haswrite = new User();
+		haswrite.setUsername("haswrite");
+		haswrite.setPassword("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		haswrite.grantAuthority("resolution:write");
+		this.userRepository.save(haswrite);
 	}
 }
--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,92 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity(name="users")
 public class User implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	
+	@Column
+	private String username;
+	
+	@Column
+	private String password;
+	
+	@Column
+	private boolean enabled = true;
+
+    @OneToMany(mappedBy = "user", fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	private Collection<UserAuthority> userAuthorities = new ArrayList<UserAuthority>();
+	
+	public User() {}
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public User(User user) {
+		this.id = user.id;
+		this.username = user.username;
+		this.password = user.password;
+		this.userAuthorities = user.userAuthorities;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	public Collection<UserAuthority> getUserAuthorities() {
+		return Collections.unmodifiableCollection(this.userAuthorities);
+	}
+	
+	public void grantAuthority(String authority) {
+		UserAuthority userAuthority = new UserAuthority(this, authority);
+		this.userAuthorities.add(userAuthority);
+	}
 }
